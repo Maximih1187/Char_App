8ae882c6dd1afefdb9edd255333bd26e6c7d00be
MaximB <maximih1108@MacBook-Air-Maksim.local>
2024-04-20 09:48:02 +0300

    ghh

--- src/reducer/ReducerCastomer.js
+++ src/reducer/ReducerCastomer.js
@@ -1,15 +1,15 @@
-const defaultState = []
+// const defaultState = []
 
 
 
-export const reducerCastomer = (state = defaultState, action) => {
-      switch (action.type) {
-            case "ADD_CASTOMER":
-                  return { ...state, }
-            case "ADD_CASTOMERS":
-                  return { ...state, }
-            default:
-                  return state
-      }
+// export const reducerCastomer = (state = defaultState, action) => {
+//       switch (action.type) {
+//             case "ADD_CASTOMER":
+//                   return { ...state, action.payload }
+//             case "ADD_CASTOMERS":
+//                   return { ...state, action.payload }
+//             default:
+//                   return state;
+//       }
 
-}
+// }

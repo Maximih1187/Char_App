506a4856406b90b7c865d6936fbd636c36b025c7
Maximka <benko_0287.mail.ru>
2024-04-21 16:11:15 +0300

    111

--- src/reducer/ReducerCastomer.js
+++ src/reducer/ReducerCastomer.js
@@ -5,9 +5,9 @@ const defaultState = []
 export const reducerCastomer = (state = defaultState, action) => {
       switch (action.type) {
             case "ADD_CASTOMER":
-                  return { ...state, action.payload }
+                  return { ...state, state: action.payload }
             case "ADD_CASTOMERS":
-                  return { ...state, action.payload }
+                  return { ...state, state: action.payload }
             default:
                   return state;
       }
